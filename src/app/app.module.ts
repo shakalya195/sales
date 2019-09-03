22aef0c28e0e56ca085d06ce596273561e8a42b9
shakalya <shakalya@code-brew.com>
2019-09-03 12:34:31 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
 import { BlockOneComponent } from './blocks/block-one/block-one.component';
 import { BlockTwoComponent } from './blocks/block-two/block-two.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: '',  redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/404' },
+]
 
 @NgModule({
   declarations: [
@@ -16,11 +24,12 @@ import { BlockTwoComponent } from './blocks/block-two/block-two.component';
     HeaderComponent,
     FooterComponent,
     BlockOneComponent,
-    BlockTwoComponent
+    BlockTwoComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
