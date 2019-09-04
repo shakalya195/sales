c65e1b322d13750d3b4d7e8d64d0fad61d16b9e8
shakalya <shakalya@code-brew.com>
2019-09-04 21:29:41 +0530

    add

--- src/app/layout/dashboard/dashboard.component.ts
+++ src/app/layout/dashboard/dashboard.component.ts
@@ -8,13 +8,18 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(
-    private api:ApiService
-  ) { }
-
   filterTabs:any[];
   levelTabs:any[];
   dashboardData:any;
+  userDataTop:any[];
+  users:any[];
+
+  filterTab:any;
+  levelTab:any;
+
+  constructor(
+    private api:ApiService
+  ) { }
 
   ngOnInit() {
     this.filterTabs = [
@@ -24,10 +29,13 @@ export class DashboardComponent implements OnInit {
       {label:'LAST QUARTER'},
       {label:'THIS YEAR'},
     ];
+    this.filterTab='TODAY'
+
     this.levelTabs = [
       {label:'TOP'},
       {label:'BOTTOM'},
     ];
+    this.levelTab= 'TOP';
 
     this.dashboardData={
       funnelData:{
@@ -58,40 +66,77 @@ export class DashboardComponent implements OnInit {
           total:100000
         },
       ],
-      topUserData:[
-        {
-          name:'John Doe',
-          newMrr:5230,
-          newLogos:102,
-          demoCalls:30
-        },
-        {
-          name:'Jane Smith',
-          newMrr:4586,
-          newLogos:95,
-          demoCalls:23
-        },
-        {
-          name:'Ethan Hunt',
-          newMrr:3500,
-          newLogos:82,
-          demoCalls:10
-        },
-      ]
     }
 
-    this.api.getData('/dashboard',{}).subscribe(r=>{
+
+    this.userDataTop = [
+      {
+        name:'John Doe',
+        newMrr:5230,
+        newLogos:102,
+        demoCalls:30
+      },
+      {
+        name:'Jane Smith',
+        newMrr:4586,
+        newLogos:95,
+        demoCalls:23
+      },
+      {
+        name:'Ethan Hunt',
+        newMrr:3500,
+        newLogos:82,
+        demoCalls:10
+      },
+    ]
+
+
+
+  }
+
+  getList(){
+    let input:any={};
+    let today = new Date();
+    if(this.filterTab){
+      switch(this.filterTab){
+        case 'LAST WEEK':
+          input.startDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
+          input.endDate = today
+          break;
+        case 'LAST MONTH':
+          input.startDate = new Date(today.getFullYear(), today.getMonth() -1 , today.getDate());
+          input.endDate = new Date(today.getFullYear(), today.getMonth() -1 , today.getDate());
+          break;
+        case 'LAST QUARTER':
+          input.startDate = new Date(today.getFullYear(), today.getMonth() -1 , today.getDate());
+          input.endDate = new Date(today.getFullYear(), today.getMonth() -1 , today.getDate());
+          break;
+        case 'THIS YEAR':
+          input.startDate = new Date(today.getFullYear(), today.getMonth() -1 , today.getDate());
+          input.endDate = new Date(today.getFullYear(), today.getMonth() -1 , today.getDate());
+          break;
+        default: 
+          input.startDate = today
+          input.endDate = today
+      }
+    }
+    console.log(input);
+    this.api.getData('/dashboard',input).subscribe(r=>{
       this.dashboardData = {};
     });
-
   }
 
   filterTabsChange(event){
     console.log(event);
+    this.filterTab = event;
+    this.getList();
   }
 
   levelTabsChange(event){
     console.log(event);
+    this.levelTab = event;
+    this.getList();
   }
+
 
 }
